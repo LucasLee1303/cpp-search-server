fc2d6d8384396475823d9614dd220ee04fad99e0
LucasLee1303 <114910809+LucasLee1303@users.noreply.github.com>
2022-10-03 22:35:00 +0300

    Update main.cpp

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,4 +1,41 @@
 // Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
 // Напишите ответ здесь:
+#include <iostream>
+#include <conio.h>
+#include <ctime>
 
+
+using namespace std;
+
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	
+	cout << "Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3? " << endl;
+
+	int kol = 0;
+			
+	for (int i = 0; i < 1000; i++)
+	{
+		
+		int units = i % 10;
+		int tens = (i / 10) % 10;
+		int hundreds = i / 100;
+
+		if (units == 3 || tens == 3 || hundreds == 3)
+		{
+			kol++;
+			
+		}
+
+	}
+	cout << "Ответ на загадку: " << kol << endl;
+
+	
+
+	_getch();
+}
+// закомитила и отправила в файле "riddle.txt", получается дублирую редактируя файл на githube
 // Закомитьте изменения и отправьте их в свой репозиторий.
