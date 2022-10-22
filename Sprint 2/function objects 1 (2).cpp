cda1b47bd2f20a32bda9f144c66a05580a506418
LucasLee1303 <114910809+LucasLee1303@users.noreply.github.com>
2022-10-22 08:18:56 -0700

    Create function objects 1 (2).cpp

---
+++ Sprint 2/function objects 1 (2).cpp
@@ -0,0 +1,75 @@
+/*
+Задание 2
+Мы ловко упорядочили животных по убыванию веса, отсортировав по возрастанию -weight. Но с именем так не выйдет. 
+Добавьте в функцию необязательный третий параметр reverse. Если туда будет передано true, объекты нужно отсортировать в обратном порядке.
+Не используйте функцию reverse из библиотеки <algorithm>.
+Пример
+int main() {
+    vector<Animal> animals = {
+        {"Мурка"s,   10, 5},
+        {"Белка"s,   5,  1.5},
+        {"Георгий"s, 2,  4.5},
+        {"Рюрик"s,   12, 3.1},
+    };
+    PrintNames(animals);
+    SortBy(animals, [](const Animal& animal) { return animal.name; }, true);
+    PrintNames(animals);
+    SortBy(animals, [](const Animal& animal) { return animal.weight; });
+    PrintNames(animals);
+    return 0;
+} 
+Вывод
+Мурка Белка Георгий Рюрик 
+Рюрик Мурка Георгий Белка 
+Белка Рюрик Георгий Мурка 
+
+Здесь будет достаточно любого решения, даже с небольшим копированием кода. Например, можно начать функцию с if (reverse) и 
+в каждой ветке отсортировать с нужным компаратором.
+*/
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Animal {
+    string name;
+    int age;
+    double weight;
+};
+
+template <typename Container, typename KeyMapper>
+void SortBy(Container& container, KeyMapper key_mapper, bool reverse = 0 /*or = false*/) {
+    if (reverse) {
+    sort (container.begin(), container.end(), [key_mapper](const auto& lhs, const auto& rhs){return key_mapper(lhs) > key_mapper(rhs);});
+    }
+     
+   else {
+   sort (container.begin(), container.end(), [key_mapper](const auto& lhs, const auto& rhs){return key_mapper(lhs) < key_mapper(rhs);});
+   }    
+    }
+    
+void PrintNames(const vector<Animal>& animals) {
+    for (const Animal& animal : animals) {
+        cout << animal.name << ' ';
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<Animal> animals = {
+        {"Мурка"s, 10, 5},
+        {"Белка"s, 5, 1.5},
+        {"Георгий"s, 2, 4.5},
+        {"Рюрик"s, 12, 3.1},
+    };
+
+    PrintNames(animals);
+    SortBy(animals, [](const Animal& animal) { return animal.name; }, true);
+    PrintNames(animals);
+    
+    SortBy(animals, [](const Animal& animal) { return animal.weight; });
+    PrintNames(animals);
+    
+    return 0;
+}
