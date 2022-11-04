be92046e806444453e36655416116720a79fabda
LucasLee1303 <114910809+LucasLee1303@users.noreply.github.com>
2022-11-04 23:22:18 +0300

    Create framwork _unit test.cpp

---
+++ Sprint 3/framwork _unit test.cpp
@@ -0,0 +1,114 @@
+/*
+Задание
+Замените встроенный макрос assert на макросы ASSERT_EQUAL, ASSERT_EQUAL_HINT, ASSERT и ASSERT_HINT из вашего фреймворка. 
+Вам пригодится решение задания из урока «Юнит-тестирование класса». Используйте его в качестве заготовки кода.
+
+Миссия выполнима. С этим заданием вы справитесь и без подсказки.
+
+ДА ХЗ, тупо прошло решение с юнит тестирования
+*/
+
+// Скопируйте сюда решения задания из урока «Юнит-тестирование класса».
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+class Synonyms {
+public:
+    void Add(const string& first_word, const string& second_word) {
+        synonyms_[first_word].insert(second_word);
+        synonyms_[second_word].insert(first_word);
+    }
+
+    size_t GetSynonymCount(const string& word) const {
+        if (synonyms_.count(word) != 0) {
+            return synonyms_.at(word).size();
+        }
+        return 0;
+    }
+
+    bool AreSynonyms(const string& first_word, const string& second_word) {
+		
+		if (synonyms_ [first_word].count(second_word) == 1)
+            return true;
+        else return false;
+
+	
+	}
+private:
+    map<string, set<string>> synonyms_;
+};
+
+void TestAddingSynonymsIncreasesTheirCount() {
+    Synonyms synonyms;
+    assert(synonyms.GetSynonymCount("music"s) == 0);
+    assert(synonyms.GetSynonymCount("melody"s) == 0);
+
+    synonyms.Add("music"s, "melody"s);
+    assert(synonyms.GetSynonymCount("music"s) == 1);
+    assert(synonyms.GetSynonymCount("melody"s) == 1);
+
+    synonyms.Add("music"s, "tune"s);
+    assert(synonyms.GetSynonymCount("music"s) == 2);
+    assert(synonyms.GetSynonymCount("tune"s) == 1);
+    assert(synonyms.GetSynonymCount("melody"s) == 1);
+}
+
+void TestAreSynonyms() {
+  
+    Synonyms synonyms;
+
+	assert(synonyms.AreSynonyms("music"s, "melody") == 1);
+	assert(synonyms.AreSynonyms("melody"s, "music") == 1);
+	synonyms.Add("music"s, "melody"s);
+	assert(synonyms.AreSynonyms("music"s, "melody") == 1);
+	assert(synonyms.AreSynonyms("melody"s, "music") == 1);
+
+	synonyms.Add("music"s, "tune"s);
+	assert(synonyms.AreSynonyms("music"s, "tune") == 0);
+	assert(synonyms.AreSynonyms("music"s, "melody") == 1);
+	assert(synonyms.AreSynonyms("music"s, "tune") == 0);
+}
+
+void TestSynonyms() {
+    TestAddingSynonymsIncreasesTheirCount();
+    TestAreSynonyms();
+}
+
+int main() {
+    TestSynonyms();
+
+    Synonyms synonyms;
+
+    string line;
+    while (getline(cin, line)) {
+        istringstream command(line);
+        string action;
+        command >> action;
+
+        if (action == "ADD"s) {
+            string first_word, second_word;
+            command >> first_word >> second_word;
+            synonyms.Add(first_word, second_word);
+        } else if (action == "COUNT"s) {
+            string word;
+            command >> word;
+            cout << synonyms.GetSynonymCount(word) << endl;
+        } else if (action == "CHECK"s) {
+            string first_word, second_word;
+            command >> first_word >> second_word;
+            if (synonyms.AreSynonyms(first_word, second_word)) {
+                cout << "YES"s << endl;
+            } else {
+                cout << "NO"s << endl;
+            }
+        } else if (action == "EXIT"s) {
+            break;
+        }
+    }
+}
