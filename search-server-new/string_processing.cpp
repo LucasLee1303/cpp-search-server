1719770f84b4a90d67c2df6041f781ee298e68e3
LucasLee1303 <114910809+LucasLee1303@users.noreply.github.com>
2023-02-15 13:51:04 +0300

    Update string_processing.cpp

--- search-server-new/string_processing.cpp
+++ search-server-new/string_processing.cpp
@@ -1,27 +1,8 @@
 #include "string_processing.h"
 
-std::vector<std::string> SplitIntoWords(const std::string& text) {
-    std::vector<std::string> words;
-    std::string word;
-    for (const char c : text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
-        }
-        else {
-            word += c;
-        }
-    }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
-    return words;
-}
-
-std::vector<std::string_view> SplitIntoWordsSV(std::string_view str) {
+std::vector<std::string_view> SplitIntoWords(std::string_view str) {
     std::vector<std::string_view> result;
+
     str.remove_prefix(std::min(str.find_first_not_of(' '), str.size()));
     const std::string_view::size_type pos_end = std::string_view::npos;
 
